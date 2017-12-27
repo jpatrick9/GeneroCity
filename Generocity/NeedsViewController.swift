716fc5bd6323276ca218f24ecc47a14d330692ea
Dante Lacey-Thompson <dlacey-thompson@csumb.edu>
2017-12-27 11:07:35 -0800

    adding needs functionality and editing place needs functionality

--- Generocity/NeedsViewController.swift
+++ Generocity/NeedsViewController.swift
@@ -17,6 +17,7 @@ class NeedsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     var needsPlace: GooglePlace!
     var needsList = [String]()
     var filteredNeedsList = [String]()
+    var needsDictionary = [String: String]()
     var inSearchMode = false
     let ref = Database.database().reference()
 
@@ -33,11 +34,16 @@ class NeedsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             var newNeeds: [String] = []
             for item in snapshot.children{
                 let itemSnap = item as! DataSnapshot
+                let itemId = itemSnap.key //the id of each need
                 let need = itemSnap.value
                 print("need: \(need ?? "need for shelter")")
                 newNeeds.append(need as! String)
+                self.needsDictionary.updateValue(need as! String, forKey: itemId)
             }
+//            print(self.needsDictionary)
             self.needsList = newNeeds
+            self.needsPlace.placeNeeds = newNeeds
+            self.filteredNeedsList = newNeeds
             self.tableView.reloadData()
         })
         
@@ -64,6 +70,14 @@ class NeedsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return inSearchMode ? filteredNeedsList.count : needsList.count
     }
     
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    
+//    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+//        return .delete
+//    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "NeedCell", for: indexPath) as? NeedCell{
             cell.configCell(gPlace: needsPlace, need: inSearchMode ? filteredNeedsList[indexPath.row] : needsList[indexPath.row])
@@ -73,4 +87,47 @@ class NeedsViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //make an alert to do delete functionality
+        let needAtPlace = needsList[indexPath.row]
+        let alertController = UIAlertController(title: "\(needsPlace.name) \(needAtPlace)", message: "Would you like to confirm drop off for this need at \(needsPlace.name)", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default, handler: {action in
+            self.deleteNeedFromPlace(needToDelete: needAtPlace)
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(_) in})
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func deleteNeedFromPlace(needToDelete: String){
+        print("In need delete and search function")
+        let needKey = self.needsDictionary.someKey(forValue: needToDelete)
+//        print(self.ref.child("place-list").child(needsPlace.placeID).child("needs").child(needKey!))
+//        self.ref.child("place-list").child(needsPlace.placeID).child("needs").child(needKey!).observeSingleEvent(of: .value, with: {(snapshot) in
+//            let val = snapshot.value as! String
+//            print("snapshot val: \(val)")
+//        })
+        self.ref.child("place-list").child(needsPlace.placeID).child("needs").child(needKey!).removeValue(completionBlock: {(error, refer) in
+                if error != nil{
+                    print(error ?? "error removing value from database")
+                }else{
+                    print(refer)
+                    print("child removed correctly")
+            }
+            })
+    }
+    
+}
+
+extension Dictionary where Value: Equatable {
+    func allKeys(forValue val: Value) -> [Key] {
+        return self.filter { $1 == val }.map { $0.0 }
+    }
+}
+
+extension Dictionary where Value: Equatable {
+    func someKey(forValue val: Value) -> Key? {
+        return first(where: { $1 == val })?.key
+    }
 }
